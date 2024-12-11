debd2bd36f13c6fe874c184fb1f935c98729c532
Juan José Gamboa Gtz <juanjgamboagtz@gmail.com>
2024-12-10 22:19:40 -0600

    prefetchQuery, useQueryClient

--- src/issues/components/IssueItem.tsx
+++ src/issues/components/IssueItem.tsx
@@ -1,6 +1,9 @@
 import { FiInfo, FiMessageSquare, FiCheckCircle } from 'react-icons/fi';
 import { useNavigate } from 'react-router-dom';
 import { GitHubIssue, IssueState } from '../../interface/issues.interface';
+import { useQueryClient } from '@tanstack/react-query';
+import { githubGetIssue } from '../../actions/get-issue.action';
+import { githubGetComments } from '../../actions/get-comments.action';
 
 interface Props {
   issue: GitHubIssue
@@ -9,8 +12,25 @@ interface Props {
 export const IssueItem = ({ issue }: Props) => {
   const navigate = useNavigate();
   const { state, number, title, user, comments } = issue;
+  const queryClient = useQueryClient()
+
+  function preFetchData() {
+    console.log('number', number)
+    queryClient.prefetchQuery({
+      queryKey: ["issue", issue.number],
+      queryFn: ({ signal }) => githubGetIssue({ id: issue.number, signal }),
+      staleTime: 1000 * 60
+    });
+
+    queryClient.prefetchQuery({
+      queryKey: ["issue", issue.number, "comments"],
+      queryFn: ({ signal }) => githubGetComments({ id: issue.number, signal }),
+      staleTime: 1000 * 60
+    });
+  }
+
   return (
-    <div className="flex items-center px-2 py-3 mb-5 border rounded-md bg-slate-900 hover:bg-slate-800">
+    <div onMouseEnter={preFetchData} className="flex items-center px-2 py-3 mb-5 border rounded-md bg-slate-900 hover:bg-slate-800">
 
       {
         state === IssueState.Closed ? <FiCheckCircle size={30} color="green" className="min-w-10" /> : <FiInfo size={30} color="red" className="min-w-10" />
